5063e0f539b3be398e7b55a069db574a50e2fd9f
elencus <richard.stephens.15@ucl.ac.uk>
2022-12-19 09:11:25 +0000

    Backup before changes

--- lennon_wall/src/lennon_wall/lennon_wall.py
+++ lennon_wall/src/lennon_wall/lennon_wall.py
@@ -2,8 +2,12 @@
 # imports
 import json
 import gensim
+import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+from scipy.stats import skew
 from sklearn.decomposition import PCA
+from sklearn.preprocessing import power_transform
 
 
 def get_character_colors():
@@ -108,7 +112,7 @@ def generate_word_embeddings():
         characters += [c for c in string]
 
     # Train the word2vec model
-    model = gensim.models.Word2Vec([characters], vector_size=100)
+    model = gensim.models.Word2Vec([characters], vector_size=300)
 
     # Save the word embeddings in a CSV file
     with open("embeddings.csv", "w", encoding="utf-8") as f:
@@ -116,7 +120,7 @@ def generate_word_embeddings():
             f.write(f"{character},{','.join(str(x) for x in embedding)}\n")
 
 
-def reduce_embeddings_to_hsl():
+def reduce_embeddings_to_hsl() -> list:
     """
     Reduces the word embeddings to 3 dimensions using PCA
     and normalizes the values to fit between 0-360 for hue,
@@ -129,39 +133,67 @@ def reduce_embeddings_to_hsl():
     pca = PCA(n_components=3)
     reduced_vectors = pca.fit_transform(df)
 
-    # Normalize the first value to fit between 0-360. Normalize the second two values to fit between 50-100
-    normalized_vectors = []
-    for vector in reduced_vectors:
-        normalized_vector = [
-            (vector[0] - reduced_vectors[:, 0].min())
-            / (reduced_vectors[:, 0].max() - reduced_vectors[:, 0].min())
-            * 360,
-            (vector[1] - reduced_vectors[:, 1].min())
-            / (reduced_vectors[:, 1].max() - reduced_vectors[:, 1].min())
-            * 50
-            + 50,
-            (vector[2] - reduced_vectors[:, 1].min())
-            / (reduced_vectors[:, 1].max() - reduced_vectors[:, 1].min())
-            * 50
-            + 50,
-        ]
-        normalized_vectors.append(normalized_vector)
+    # Compare the skewness of the data. Swap the dimensions so that
+    # the hue dimension (dimension 0) is the least skewed.
+    if skew(reduced_vectors[:, 0]) > skew(reduced_vectors[:, 1]):
+        reduced_vectors = np.array(reduced_vectors)[:, [1, 0, 2]]
+    if skew(reduced_vectors[:, 0]) > skew(reduced_vectors[:, 2]):
+        reduced_vectors = np.array(reduced_vectors)[:, [2, 1, 0]]
 
-    # Create a new DataFrame with the normalized vectors and the same index as the original DataFrame. Also, rename the columns to 'hue', 'saturation', and 'lightness'
+    # Copmare the skewness of the data. Swap the dimensions so that
+    # the saturation dimension (dimension 1) is the most skewed.
+    if skew(reduced_vectors[:, 1]) < skew(reduced_vectors[:, 2]):
+        reduced_vectors = np.array(reduced_vectors)[:, [0, 2, 1]]
+
+    # Swap the skewness of the saturation dimension if it is skewed positively.
+    if skew(reduced_vectors[:, 1]) > 0:
+        reduced_vectors[:, 1] = -reduced_vectors[:, 1]
+
+    # Normalize the saturation and lightness values to fit between 50-100.
+    # Normalize the hue values to fit between 0-360.
+    normalized_hsl_vectors = np.array(reduced_vectors)
+    normalized_hsl_vectors[:, 0] = (
+        (normalized_hsl_vectors[:, 0] - normalized_hsl_vectors[:, 0].min())
+        / (normalized_hsl_vectors[:, 0].max() - normalized_hsl_vectors[:, 0].min())
+        * 360
+    )
+    normalized_hsl_vectors[:, 1] = (
+        (normalized_hsl_vectors[:, 1] - normalized_hsl_vectors[:, 1].min())
+        / (normalized_hsl_vectors[:, 1].max() - normalized_hsl_vectors[:, 1].min())
+        * 100
+    )
+    normalized_hsl_vectors[:, 2] = (
+        normalized_hsl_vectors[:, 2] - normalized_hsl_vectors[:, 2].min()
+    ) / (
+        normalized_hsl_vectors[:, 2].max() - normalized_hsl_vectors[:, 2].min()
+    ) * 50 + 50
+
+    # Print a graph that shows the distribution of
+    # the hue, saturation, and lightness values in a 3x1 grid.
+    # Hue should have 360 bins, saturation and lightness should have 50 bins.
+    fig, (ax1, ax2, ax3) = plt.subplots(3, 1)
+    ax1.hist(normalized_hsl_vectors[:, 0], bins=360)
+    ax2.hist(normalized_hsl_vectors[:, 1], bins=100)
+    ax3.hist(normalized_hsl_vectors[:, 2], bins=50)
+    plt.show()
+
+    # Create a new DataFrame with the normalized vectors and the same index
+    # as the original DataFrame. Also, rename the columns to 'hue', 'saturation', and 'lightness'
     normalized_df = pd.DataFrame(
-        normalized_vectors, index=df.index, columns=["hue", "saturation", "lightness"]
+        normalized_hsl_vectors,
+        index=df.index,
+        columns=["hue", "saturation", "lightness"],
     )
 
     # Save the normalized vectors to a new csv file called hsl.csv
-    normalized_df.to_csv("hsl.csv")
+    normalized_df.to_csv(f"hsl.csv")
 
 
 def main():
     """This function runs the main code for the lennon_wall package."""
-    for i in range(10):
-        generate_word_embeddings()
-        reduce_embeddings_to_hsl()
-        create_svg(i)
+    generate_word_embeddings()
+    reduce_embeddings_to_hsl()
+    create_svg()
 
 
 # main
